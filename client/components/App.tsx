29160d405be9186f3c6559dd075cbd05016f6c5f
student <student@pop-os.localdomain>
2023-03-17 11:15:46 +1300

    add/adds routes and redirect

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,15 +1,19 @@
 import Level1 from './Level1'
 import Header from './Header'
 import Footer from './Footer'
-
-import { useState } from 'react'
-
+import Test from './Test'
+import Home from './Home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <Header />
-      <Level1 />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Level1" element={<Level1 />} />
+        <Route path="/Test" element={<Test />} />
+      </Routes>
       <Footer />
     </>
   )
--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   const [colour, setColour] = useState('black')
@@ -17,8 +18,9 @@ function Footer() {
       <p onMouseEnter={colourHandler} style={{ color: { colour } }}>
         made by JAIL
       </p>
+      <Link to={'/Level1'}>to Level1</Link>
+      <Link to={'/Test'}>to Test</Link>
     </footer>
-
   )
 }
 
