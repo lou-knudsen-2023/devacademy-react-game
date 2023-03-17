636d77d4d897aa6f5303874f7bf9d43c95f302a9
lou-knudsen-2023 <121981156+lou-knudsen-2023@users.noreply.github.com>
2023-03-17 13:31:23 +1300

    have weeMan entering in shop in right position

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,10 +2,10 @@ import Centre from './Centre'
 import Header from './Header'
 import Footer from './Footer'
 import Test from './Test'
-import North from './North'
-import East from './East'
-import South from './South'
-import West from './West'
+// import North from './North'
+// import East from './East'
+// import South from './South'
+// import West from './West'
 import Shop from './Shop'
 import Home from './Home'
 
@@ -20,10 +20,10 @@ function App() {
         <Route path="/Centre" element={<Centre />} />
         <Route path="/Test" element={<Test />} />
         <Route path="/Shop" element={<Shop />} />
-        <Route path="/North" element={<North />} />
-        <Route path="/East" element={<East />} />
-        <Route path="/South" element={<South />} />
-        <Route path="/West" element={<West />} />
+        {/* <Route path="/North" element={<North />} /> */}
+        {/* <Route path="/East" element={<East />} /> */}
+        {/* <Route path="/South" element={<South />} /> */}
+        {/* <Route path="/West" element={<West />} /> */}
       </Routes>
       <Footer />
     </>
